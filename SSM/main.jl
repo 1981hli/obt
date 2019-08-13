535ce61013262eaca978d7d695340947b194fd38
LiHuan <1981hli@gmail.com>
2019-08-13 15:44:39 +0800

    modified:   main.jl 	modified:   out/txyz.svg

--- SSM/main.jl
+++ SSM/main.jl
@@ -17,7 +17,7 @@ const s_Day=1/Day_s
 const m_AU=1/AU_m
 const kg_Earthmass=1/Earthmass_kg
 const TotalBody=11
-const TotalStep=365*5
+const TotalStep=365
 const TimeInterval=1.
 const BeginTime=2440400.5 # 1969.06.28
 const c=299792458*m_AU*s_Day^-1
@@ -76,20 +76,16 @@ end
 
 #-------------------------------------------------------------------------------
 
-function gravity_Newton_by1(test::Body,source::Body)
-  dx=source.x-test.x
-  gravity=G*test.mass*source.mass/sqrt(sum(dx.^2))^3*dx
-end
+function gravity_Newton(step::Step,testnum::Int)
+  function gravity_Newton1(test::Body,source::Body)
+    dx=source.x-test.x
+    G*test.mass*source.mass/sqrt(sum(dx.^2))^3*dx # return the gravity
+  end
 
-
-
-
-
-function gravity_Newton_byall(step::Step,testnum::Int)
   gravity=[0.,0.,0.]
   for i in 1:TotalBody
     if i==testnum continue end
-    gravity=gravity+gravity_Newton_by1(step.body[testnum],step.body[i])
+    gravity=gravity+gravity_Newton1(step.body[testnum],step.body[i])
   end
   gravity
 end
@@ -98,48 +94,53 @@ end
 
 
 
-# function forcePPN(step::Step,A::Int)
-  # m(i::Int)=step.body[i].mass
-  # x(i::Int)=step.body[i].x
-  # v(i::Int)=step.body[i].v
-  # a(i::Int)=1/m(i)*forceNewtonbyall(step,i) # to be accelerated
+function gravity_PPN(step::Step,A::Int) # (27) in 196C
+  beta=1.
+  gamma=1.
+  m(i::Int)=step.body[i].mass
+  x(i::Int)=step.body[i].x
+  v(i::Int)=step.body[i].v
+  a(i::Int)=1/m(i)*gravity_Newton(step,i)
 
-  # r(A::Int,B::Int)=x(B)-x(A)
-  # v(A::Int,B::Int)=v(B)-v(A)
-  # norm_r(A::Int,B::Int)=norm(r(A,B))
-  # norm_v(A::Int,B::Int)=norm(v(A,B))
+  r(A::Int,B::Int)=x(B)-x(A)
+  v(A::Int,B::Int)=v(B)-v(A)
+  norm_r(A::Int,B::Int)=norm(r(A,B))
+  norm_v(A::Int,B::Int)=norm(v(A,B))
+  norm_v(A::Int)=norm(v(A))
     
-  # function forcePPNby1(step::Step,A::Int,B::Int)
-    # T1 =G*m(B)*r(A,B)/norm_r(B,A)^3
-    # T2 =-2*(beta+gamma)/c^2*
-        # sum( G*m(i)/norm_r(A,i) for i=[1:TotalBody;] if i!=A )
-    # T3 =-(2*beta-1)/c^2*
-        # sum( G*m(i)/norm_r(B,i) for i=[1:TotalBody;] if i!=B )
-    # T4 =gamma*(v(A)/c)^2
-    # T5 =(1+gamma)*(v(B)/c)^2
-    # T6 =-2(1+gamma)/c^2*dot(v(A),v(B))
-    # T7 =-3/(2*c^2)*(dot(r(B,A),v(B))/norm_r(A,B))^2
-    # T8 =1/(2*c^2)*dot(r(A,B),a(B))
-    # T9 =1/c^2*G*m(B)/norm_r(A,B)^3*
-        # dot(r(B,A),(2+2*gamma)*v(A)-(1+2*gamma)*v(B))*v(B,A)
-    # T10=(3+4*gamma)/(2*c^2)*G*m(B)/r(A,B)*a(B)
-    # acceleration=T1*(1+T2+T3+T4+T5+T6+T7+T8)+T9+T10
-  # end
+  function gravity_PPN1(step::Step,A::Int,B::Int)
+    T1 =G*m(B)*r(A,B)/norm_r(B,A)^3
+    T2 =-2*(beta+gamma)/c^2*sum( G*m(C)/norm_r(A,C) for C=1:TotalBody if C!=A )
+    T3 =-(2*beta-1)/c^2*sum( G*m(C)/norm_r(B,C) for C=1:TotalBody if C!=B )
+    T4 =gamma*(norm_v(A)/c)^2
+    T5 =(1+gamma)*(norm_v(B)/c)^2
+    T6 =-2(1+gamma)/c^2*dot(v(A),v(B))
+    T7 =-3/(2*c^2)*(dot(r(B,A),v(B))/norm_r(A,B))^2
+    T8 =1/(2*c^2)*dot(r(A,B),a(B))
+    T9 =1/c^2*G*m(B)/norm_r(A,B)^3*dot(r(B,A),(2+2*gamma)*v(A)-(1+2*gamma)*v(B))*v(B,A)
+    T10=(3+4*gamma)/(2*c^2)*G*m(B)/norm_r(A,B)*a(B)
+    T1*(1+T2+T3+T4+T5+T6+T7+T8)+T9+T10 # acceleration without sum for B
+  end
+ 
+  # return the gravity itself, not the acceleration
+  m(A)*sum(gravity_PPN1(step,A,B) for B=1:TotalBody if B!=A)
+end
 
-  # sum(forcePPNby1(step,A,B) for B=[1:TotalBody;] if B!=A)
-# end
+
+
+
+
+gravity=gravity_Newton
 
 #-------------------------------------------------------------------------------
 
 function rk_rk4(thestep::Step,dt::Float64)
-
   function rk_dstep(thestep::Step,dt::Float64)
     tmp=deepcopy(thestep)
     tmp.time=dt
     for i in 1:TotalBody
-      force=gravity_Newton_byall(thestep,i)
       tmp.body[i].x=thestep.body[i].v*dt # dx=v*dt
-      tmp.body[i].v=1/thestep.body[i].mass*force*dt # dv=a*dt
+      tmp.body[i].v=1/thestep.body[i].mass*gravity(thestep,i)*dt # dv=a*dt
     end
     tmp
   end
@@ -216,19 +217,18 @@ end
 #-------------------------------------------------------------------------------
 # plot
 
-let
+# let
+  # t=1:TotalStep
+  # x=[step[i].body[3].x[1] for i=t]
+  # y=[step[i].body[3].x[2] for i=t]
+  # z=[step[i].body[3].x[3] for i=t]
+  # xDE=[stepDE[i].body[3].x[1] for i=t]
+  # yDE=[stepDE[i].body[3].x[2] for i=t]
+  # zDE=[stepDE[i].body[3].x[3] for i=t]
 
-  t=1:TotalStep
-  x=[step[i].body[3].x[1] for i=t]
-  y=[step[i].body[3].x[2] for i=t]
-  z=[step[i].body[3].x[3] for i=t]
-
-  xlabel("step / Day")
-  ylabel("x,y,z / AU")
-  plot(t,x)
-  oplot(t,y)
-  oplot(t,z)
-  savefig("out/txyz.svg")
-
-end
+  # xlabel("step / Day")
+  # ylabel("x,y,z / AU")
+  # plot(t,xDE)
+  # savefig("out/txyz.svg")
+# end
 
